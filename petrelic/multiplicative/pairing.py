26cf769c4b50bff15ccf18658360fb8849e75cf7
Wouter Lueks <wouter.lueks@epfl.ch>
2020-03-03 18:57:22 +0100

    First multipliciative interface

---
+++ petrelic/multiplicative/pairing.py
@@ -0,0 +1,187 @@
+"""Additive API of the petrelic library.
+"""
+
+import msgpack
+import petlib.pack as pack
+
+from petrelic.bindings import _FFI, _C
+from petrelic.bn import Bn, force_Bn_other
+
+from petrelic.pairing import G2
+from petrelic.pairing import G2Element
+from petrelic.pairing import NoAffineCoordinateForECPoint
+
+import petrelic.pairing as basepairing
+
+
+class BilinearGroupPair:
+    """
+    A bilinear group pair.
+
+    Contains two origin groups G1, G2 and the image group Gt.
+    """
+
+    def __init__(self):
+        """Initialise a bilinear group pair."""
+        self.GT = Gt()
+        self.G1 = G1()
+        self.G2 = G2()
+
+    def groups(self):
+        """
+        Returns the three groups in the following order :  G1, G2, Gt.
+        """
+        return self.G1, self.G2, self.GT
+
+
+class G1(basepairing.G1):
+    """G1 group."""
+
+    @classmethod
+    def element_type(cls):
+        return G1Element
+
+    @classmethod
+    def prod(cls, elems):
+        """Efficient product of a number of elements
+
+        In the current implementation this function is not optimized.
+
+        Example:
+            >>> elems = [ G1.get_generator() ** x for x in [10, 25, 13]]
+            >>> G1.prod(elems) ==  G1.get_generator() ** (10 + 25 + 13)
+            True
+        """
+        res = cls.get_neutral_element()
+        for el in elems:
+            res *= el
+        return res
+
+    @classmethod
+    def wprod(cls, weights, elems):
+        """Efficient weighted product of a number of elements
+
+        In the current implementation this function is not optimized.
+        Example:
+            >>> weights = [1, 2, 3]
+            >>> elems = [ G1.get_generator() ** x for x in [10, 25, 13]]
+            >>> G1.wprod(weights, elems) ==  G1.get_generator() ** (1 * 10 + 2 * 25 + 3 * 13)
+            True
+        """
+        res = cls.get_neutral_element()
+        for w, el in zip(weights, elems):
+            res *= el ** w
+
+        return res
+
+
+class G1Element(basepairing.G1Element):
+    """Element of the G1 group."""
+
+    group = G1
+
+    def pair(self, other):
+        res = GtElement()
+        _C.pc_map(res.pt, self.pt, other.pt)
+        return res
+
+    square = basepairing.G1Element.double
+    isquare = basepairing.G1Element.idouble
+
+    __mul__ = basepairing.G1Element.__add__
+    __rmul__ = basepairing.G1Element.__radd__
+    __imul__ = basepairing.G1Element.__iadd__
+
+    __truediv__ = basepairing.G1Element.__sub__
+    __rtruediv__ = basepairing.G1Element.__rsub__
+    __itruediv__ = basepairing.G1Element.__isub__
+
+    __pow__ = basepairing.G1Element.__mul__
+    __rpow__ = basepairing.G1Element.__rmul__
+    __ipow__ = basepairing.G1Element.__imul__
+
+    mul = __mul__
+    imul = __imul__
+    truediv = __truediv__
+    itruediv = __itruediv__
+    __floordiv__ = __truediv__
+    __ifloordiv__ = __itruediv__
+    pow = __pow__
+    ipow = __ipow__
+
+
+class G2(basepairing.G2):
+    @classmethod
+    def element_type(cls):
+        return G2Element
+
+
+class G2Element(basepairing.G2Element):
+    """Element of the G2 group."""
+
+    group = G2
+
+    square = basepairing.G2Element.double
+    isquare = basepairing.G2Element.idouble
+
+    __mul__ = basepairing.G2Element.__add__
+    __rmul__ = basepairing.G2Element.__radd__
+    __imul__ = basepairing.G2Element.__iadd__
+
+    __truediv__ = basepairing.G2Element.__sub__
+    __rtruediv__ = basepairing.G2Element.__rsub__
+    __itruediv__ = basepairing.G2Element.__isub__
+
+    __pow__ = basepairing.G2Element.__mul__
+    __rpow__ = basepairing.G2Element.__rmul__
+    __ipow__ = basepairing.G2Element.__imul__
+
+    mul = __mul__
+    imul = __imul__
+    truediv = __truediv__
+    itruediv = __itruediv__
+    __floordiv__ = __truediv__
+    __ifloordiv__ = __itruediv__
+    pow = __pow__
+    ipow = __ipow__
+
+
+class Gt(basepairing.Gt):
+    """Gt group."""
+
+    @classmethod
+    def element_type(cls):
+        return GtElement
+
+    get_infinity = basepairing.Gt.get_neutral_element
+    infinite = basepairing.Gt.get_neutral_element
+
+
+class GtElement(basepairing.GtElement):
+    """Gt element."""
+
+    group = Gt
+
+
+def pt_enc(obj):
+    """Encoder for the wrapped points."""
+    data = obj.to_binary()
+    packed_data = msgpack.packb(data)
+    return packed_data
+
+
+def pt_dec(bptype):
+    """Decoder for the wrapped points."""
+
+    def dec(data):
+        data_extracted = msgpack.unpackb(data)
+        pt = bptype.from_binary(data_extracted)
+        return pt
+
+    return dec
+
+
+# Register encoders and decoders for pairing points
+# pack.register_coders(G1Element, 118, pt_enc, pt_dec(G1Element))
+# pack.register_coders(G2Element, 119, pt_enc, pt_dec(G2Element))
+# pack.register_coders(GtElement, 120, pt_enc, pt_dec(GtElement))
---
+++ tests/test_multiplicative_pairing.py
@@ -0,0 +1,137 @@
+import pytest
+
+from petrelic.multiplicative.pairing import (
+    G1,
+    G1Element,
+    G2,
+    G2Element,
+    Gt,
+    GtElement,
+    NoAffineCoordinateForECPoint,
+)
+from petrelic.bn import Bn
+
+
+@pytest.fixture(params=[G1, G2, Gt])
+def generator(request):
+    return request.param.get_generator()
+
+
+@pytest.fixture(params=[G1, G2, Gt])
+def group(request):
+    return request.param
+
+
+@pytest.fixture(params=[G1Element, G2Element, GtElement])
+def element(request):
+    return request.param
+
+
+def test_order(group):
+    g = group.get_generator()
+    o = group.order()
+    assert g ** o == group.get_neutral_element()
+
+
+def test_square_small(generator):
+    a = 10
+    h = generator ** a
+    assert h.square() == generator ** (2 * a)
+
+
+def test_square_small(generator):
+    a = 10
+    h = generator ** a
+    h.isquare()
+    assert h == generator ** (2 * a)
+
+
+def test_square_random(group):
+    g = group.get_generator()
+    a = group.order().random()
+    h = g ** a
+    assert h.square() == g ** (2 * a)
+
+
+def test_isquare_random(group):
+    g = group.get_generator()
+    a = group.order().random()
+    h = g ** a
+    h.isquare()
+    assert h == g ** (2 * a)
+
+
+def test_mul_random(group):
+    g = group.get_generator()
+    a = group.order().random()
+    b = group.order().random()
+    h = g ** a
+    k = g ** b
+    assert h * k == g ** (a + b)
+
+
+def test_mul_large(group):
+    g = group.get_generator()
+    a = group.order() - 1
+    b = group.order() - 2
+    h = g ** a
+    k = g ** b
+    assert h * k == g ** -3
+
+
+def test_imul(group):
+    g = group.get_generator()
+    a = group.get_order().random()
+    b = group.get_order().random()
+    h = g ** a
+    k = g ** b
+    assert h * k == g ** (a + b)
+    h.imul(k)
+    assert h == g ** (a + b)
+
+
+def test_mul_pow(group):
+    g = group.get_generator()
+    assert g * g == g ** 2
+
+    h = g ** group.get_order().random()
+    assert h * h * h == h ** 3
+
+
+def test_pow_identity(group):
+    g = group.get_generator()
+    assert group.get_neutral_element() == g ** 0
+
+
+def test_pow_inverse(group):
+    g = group.get_generator()
+    ginv = g.inverse()
+    assert ginv == g ** (-1)
+
+
+def test_mul_different_type(group):
+    g = group.get_generator()
+    o = group.order()
+    with pytest.raises(TypeError):
+        g * o
+
+
+def test_inverse(group):
+    g = group.get_generator()
+    a = group.order().random()
+    h = g ** a
+
+    assert h.inverse() == group.get_neutral_element() / h
+
+    hinv = g ** (group.order() - a)
+    assert h.inverse() == hinv
+
+
+def test_iinverse(group):
+    g = group.get_generator()
+    a = group.order().random()
+    h = g ** a
+    hinv = g ** (group.order() - a)
+
+    h.iinverse()
+    assert h == hinv
